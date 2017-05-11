a2decf5a1f4455c3e10f6ebc6cb26959f30a530f
Monica <njun658@aucklanduni.ac.nz>
2017-05-11 16:00:39 +1200

    implement condition when there is more base-station

--- #80/aa.m
+++ #80/aa.m
@@ -1,16 +1,20 @@
 tmp = zeros(no_BS,1);
 tmp = [distance tmp];
-temp = zeros(no_BS,no_BS);
+temp = zeros(N,N,no_BS);
 n = 2;
-for i = 1:N
-    for j = 1:no_BS
-        a =(1/distance(j,i))^n;
-        b = 0;
-        for k = 1:no_BS
-            if (k ~= j)
-                b = b + distance(k,i);
+for l =  1:no_BS
+    this = distance;
+    this(l,:) = [];
+    for i = 1:N
+        for j = 1:N
+            a =(1/distance(j,i))^n;
+            b = 0;
+            for k = 1:N
+                if (k ~= j)
+                    b = b + this(k,i);
+                end
             end
+            temp(i,j,l) = b;
         end
-        temp(i,j) = b;
     end
 end
